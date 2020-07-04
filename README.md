432363bfb7d58f005faaeeb183608cd0e9bc699e
vitaliychoi <vitaliy.tsoy.ae@gmail.com>
2020-07-04 14:47:26 +0400

    Readme.md updated

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# This is RUBY code
 # Magic 8 ball - daily problem solver!
-# Think about the question you need to solve
-# Open magic_8_ball.rb in command prompt
-# Enjoy the decision!
+Magic 8 Ball generates random answer for your daily routine questions
+follow the link below for more details
+https://en.wikipedia.org/wiki/Magic_8-Ball
+
+How to use:
+1. Think about the question you need to solve
+2. Open magic_8_ball.rb in command prompt
+3. Enjoy the decision!
--- magic_8_ball.rb
+++ magic_8_ball.rb
@@ -1,38 +1,38 @@
 # Greetings samples
 greetings = [
-		"Wsuup dude! The ans is..",
-		"Who asks, recieves an answer:",
-		"Hey mortal. There is an answer for you today:"
+  "Wsuup dude! The ans is..",
+  "Who asks, recieves an answer:",
+  "Hey mortal. There is an answer for you today:"
 ]
 # Random answers
 answers = [
-		# Positive
-		"Certainly",
-		"Predetermined",
-		"No doubt",
-		"Definitely yes",
-		"You might be sure of it",
+  # Positive
+  "Certainly",
+  "Predetermined",
+  "No doubt",
+  "Definitely yes",
+  "You might be sure of it",
 
-		# Mid - positive
-		"I think  — «yes»",
-		"Probaly",
-		"Good perspectives",
-		"Signs say — «yes»",
-		"Yes",
+  # Mid - positive
+  "I think  — «yes»",
+  "Probaly",
+  "Good perspectives",
+  "Signs say — «yes»",
+  "Yes",
 
-		# Neutral
-		"I'm not sure, try again next time",
-		"Ask me later",
-		"I am silent now",
-		"It is not good time to predict",
-		"Focus and ask again",
+  # Neutral
+  "I'm not sure, try again next time",
+  "Ask me later",
+  "I am silent now",
+  "It is not good time to predict",
+  "Focus and ask again",
 
-		# Negative
-		"Don't think about it even",
-		"My answer — «no»",
-		"Based on my info — «no»",
-		"Not good perpectives",
-		"Highly doubtful"
+  # Negative
+  "Don't think about it even",
+  "My answer — «no»",
+  "Based on my info — «no»",
+  "Not good perpectives",
+  "Highly doubtful"
 ]
 
 puts greetings.sample
